fc7d8520b85fa40dea4e4a0c25686db67c47d1c2
Grendel <jcg@grendel.world>
2023-10-16 17:26:12 -0500

    Generate grid

--- minesweeper/game.js
+++ minesweeper/game.js
@@ -1,3 +1,15 @@
 const minefield = document.getElementById("minefield");
 
-console.log(minefield);
+const rows = 16;
+const cols = 30;
+
+for (let r=0; r<rows; r++) {
+  const row = document.createElement("div");
+  row.classList.add("cell-row");
+  minefield.appendChild(row);
+
+  for (let c=0; c<cols; c++) {
+    const cell = document.createElement("minefield-cell");
+    row.appendChild(cell);
+  }
+}
