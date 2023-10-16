fc97978681806efc4a28b4d11fe2805694143a39
Grendel <jcg@grendel.world>
2023-10-16 18:39:14 -0500

    Connect button callbacks to game logic

--- minesweeper/game.js
+++ minesweeper/game.js
@@ -3,13 +3,32 @@ const minefield = document.getElementById("minefield");
 const rows = 16;
 const cols = 30;
 
+const cells = []
+
 for (let r=0; r<rows; r++) {
   const row = document.createElement("div");
-  row.classList.add("cell-row");
   minefield.appendChild(row);
+  row.classList.add("cell-row");
 
   for (let c=0; c<cols; c++) {
     const cell = document.createElement("minefield-cell");
     row.appendChild(cell);
+    cell.setCellCoord(r, c);
+    cell.connectPressed(processNeighbors);
+    cells.push(cell);
+  }
+}
+
+function processNeighbors(neighbors) {
+  neighbors = neighbors.filter(n => {
+    return (0 <= n.row && n.row < rows) && (0 <= n.col && n.col < cols);
+  });
+  for (let n of neighbors) {
+    const idx = n.row * cols + n.col;
+    if (cells[idx].isDisabled()) {
+      continue;
+    }
+    console.log(`Pressing button ${n.row},${n.col}`);
+    setTimeout(() => cells[idx].pressButton(), 0);
   }
 }
--- minesweeper/types.js
+++ minesweeper/types.js
@@ -1,34 +1,80 @@
 if ("content" in document.createElement("template")) {
-    const cellTemplate = document.getElementById("cell-template");
+  const cellTemplate = document.getElementById("cell-template");
 
-    class MinefieldCell extends HTMLElement {
-        static cells = 0;
+  class MinefieldCell extends HTMLElement {
+    static cells = 0;
 
-        constructor() {
-            super();
-        }
+    /// Callback: Function(Array) -> Void
+    emitPressed = (arr) => { return; };
 
-        connectedCallback() {
-            MinefieldCell.cells++;
-            const clone = cellTemplate.content.cloneNode(true);
-            const div = clone.children[0];
-            div.setAttribute("data-cell", String(MinefieldCell.cells));
-            const button = div.children[0];
-            button.addEventListener("click", e => {
-                e.target.setAttribute("disabled", true);
-            });
-
-            // const shadow = this.attachShadow({ mode: "open" });
-            this.appendChild(clone);
-        }
-
-        disconnectedCallback() {
-            MinefieldCell.cells--;
-        }
+    constructor() {
+      super();
     }
 
-    customElements.define("minefield-cell", MinefieldCell);
+    connectedCallback() {
+      const clone = cellTemplate.content.cloneNode(true);
+
+      const div = clone.children[0];
+      div.setAttribute("data-cell", String(MinefieldCell.cells));
+
+      const button = div.children[0];
+      button.addEventListener("click", () => this.pressButton());
+
+      this.appendChild(clone);
+
+      MinefieldCell.cells++;
+    }
+
+    disconnectedCallback() {
+      MinefieldCell.cells--;
+    }
+
+    disable() {
+      this.getButton().setAttribute("disabled", true);
+    }
+
+    isDisabled() {
+      return this.getButton().getAttribute("disabled");
+    }
+
+    getButton() {
+      return this.children[0].children[0];
+    }
+
+    pressButton() {
+      if (this.isDisabled()) {
+        return;
+      }
+      this.disable();
+      this.returnNeighbors();
+    }
+
+    setCellCoord(row, col) {
+      const cell = [row, col].join(",");
+      this.setAttribute("data-cell", cell);
+    }
+
+    connectPressed(cb) {
+      this.emitPressed = cb;
+    }
+
+    returnNeighbors() {
+      const neighbors = [];
+      const [row, col] = this.getAttribute("data-cell").split(",").map(n => Number(n));
+      for (let r=row-1; r<=row+1; r++) {
+        for (let c=col-1; c<=col+1; c++) {
+          if (r===row && c===col) {
+            continue;
+          }
+          neighbors.push({row: r, col: c});
+        }
+      }
+      this.emitPressed(neighbors);
+    }
+  }
+
+  customElements.define("minefield-cell", MinefieldCell);
 }
 else {
-    console.error("HTML Templates are not supported");
+  console.error("HTML Templates are not supported");
 }
